00326d397379516bf3314f5dbf1ca1d8fcd8a930
chongwei-fsd <ChongWei@genstudents.org>
2024-10-06 20:12:27 +0800

    added async function fetch api

--- frontend/script/profile-loader.js
+++ frontend/script/profile-loader.js
@@ -2,128 +2,144 @@ const userContainerName = "userContainer";
 const rewardsContainerName = "rewardsContainer";
 const redeemedContainerName = "redeemedContainer";
 
-const spinnerContainerName = "spinnerContainer";
-
-const spinnerContainer = document.createElement("div");
-spinnerContainer.setAttribute("id", spinnerContainerName);
-spinnerContainer.className = "text-center brown-text fw-light mx-auto w-25 pt-4";
-spinnerContainer.innerText = " Loading...";
-
-// let userController = null;
 
 document.addEventListener("DOMContentLoaded", (event) => {
     event.preventDefault();
     fetchUserById(2); //userid2
-    fetchAllVouchers();
-    fetchRedeemedById(2); //userid2
 });
 
-function fetchUserById(id) {
+document.getElementById('myRewardsTab').addEventListener('click', myRewardsClick, fetchRedeemedById(2))
+document.getElementById('rewardsTab').addEventListener('click', rewardsClick, fetchAllVouchers())
+
+
+async function fetchUserById(id) {
     const userContainer = document.getElementById(userContainerName);
-    userContainer.append(spinnerContainer);
-    const userSpinner = new Spinner();
-    userSpinner.createSpinner(spinnerContainerName);
-    userSpinner.displaySpinner(true);
+    const userSpinner = createAndShowSpinner(userContainer);
 
-    fetch(`http://localhost:8080/api/user/${id}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parse JSON response
-        })
-        .then(data => {
-            // console.log(data); 
-            const userController = new UserController(userContainerName);
-            userController.displayUser(data);
-
-            userSpinner.displaySpinner(false);
-            userContainer.removeChild(spinnerContainer);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    try {
+        const response = await fetch(`http://localhost:8080/api/user/${id}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const userController = new UserController(userContainerName);
+        userController.displayUser(data);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    } finally {
+        removeSpinner(userContainer, userSpinner);
+    }
 }
 
 
-function fetchAllVouchers() {
+async function fetchAllVouchers() {
     const rewardsContainer = document.getElementById(rewardsContainerName);
-    rewardsContainer.append(spinnerContainer);
-    const rewardsSpinner = new Spinner();
-    rewardsSpinner.createSpinner(spinnerContainerName);
-    rewardsSpinner.displaySpinner(true);
+    const rewardsSpinner = createAndShowSpinner(rewardsContainer);
 
-    fetch(`http://localhost:8080/api/voucher`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parse JSON response
-        })
-        .then(data => {
-            // console.log(data); 
-            const voucherController = new VoucherController(rewardsContainerName);
-            voucherController.displayVouchers(data);
-
-            rewardsSpinner.displaySpinner(false);
-            rewardsContainer.removeChild(spinnerContainer);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    try {
+        const response = await fetch(`http://localhost:8080/api/voucher`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const voucherController = new VoucherController(rewardsContainerName);
+        voucherController.displayVouchers(data);
+    } catch (error) {
+        console.error('Error fetching vouchers:', error);
+    } finally {
+        removeSpinner(rewardsContainer, rewardsSpinner);
+    }
 }
 
 
-function addVoucher(userId, voucherId) {
-    const requestData = {
-        userId: userId,
-        voucherId: voucherId
-    };
+async function addVoucher(userId, voucherId) {
+    const requestData = { userId, voucherId };
 
-    fetch(`http://localhost:8080/api/redeemed/${userId}/${voucherId}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(requestData) //convert js obj to json
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parse JSON response
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+    try {
+        const response = await fetch(`http://localhost:8080/api/redeemed/${userId}/${voucherId}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(requestData)
         });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data)
+
+        myRewardsClick();
+         const redeemedController = new RedeemedController(redeemedContainerName);
+        redeemedController.displayRedeemedVouchers([data]);
+        
+    } catch (error) {
+        console.error('Error adding voucher:', error);
+    }
 }
 
-function fetchRedeemedById(userId) {
+
+async function fetchRedeemedById(userId) {
     const redeemedContainer = document.getElementById(redeemedContainerName);
-    redeemedContainer.append(spinnerContainer);
-    const redeemedSpinner = new Spinner();
-    redeemedSpinner.createSpinner(spinnerContainerName);
-    redeemedSpinner.displaySpinner(true);
+    const redeemedSpinner = createAndShowSpinner(redeemedContainer);
 
-    fetch(`http://localhost:8080/api/redeemed/${userId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parse JSON response
-        })
-        .then(data => {
-            // console.log(data.voucher);
-            const redeemedController = new RedeemedController(redeemedContainerName);
-            redeemedController.displayRedeemedVouchers(data)
-            
-            redeemedSpinner.displaySpinner(false);
-            redeemedContainer.removeChild(spinnerContainer);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    try {
+        const response = await fetch(`http://localhost:8080/api/redeemed/${userId}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const redeemedController = new RedeemedController(redeemedContainerName);
+        redeemedController.displayRedeemedVouchers(data);
+    } catch (error) {
+        console.error('Error fetching redeemed vouchers:', error);
+    } finally {
+        removeSpinner(redeemedContainer, redeemedSpinner);
+    }
 }
+
+
+//spinner create
+function createAndShowSpinner(container) {
+    let spinnerContainer = container.querySelector('.spinner');
+    if (!spinnerContainer) {
+        spinnerContainer = document.createElement('div');
+        spinnerContainer.className = 'spinner text-center brown-text fw-light mx-auto w-25 pt-4';
+        spinnerContainer.innerText = ' Loading...';
+        container.append(spinnerContainer);
+    }
+    return spinnerContainer;
+}
+
+//spinner remove
+function removeSpinner(container, spinner) {
+    if (spinner) {
+        container.removeChild(spinner);
+    }
+}
+
+
+function rewardsClick(){
+    myRewardsContent.classList='col-12 d-none'
+    rewardsContent.classList='col-12'
+
+    rewardsTab.classList.remove('bg-trans-white')
+    rewardsTab.classList.add('bg-white','fw-bold')
+
+    myRewardsTab.classList.remove('bg-white','fw-bold')
+    myRewardsTab.classList.add('bg-trans-white')  
+}
+
+function myRewardsClick(){
+    rewardsContent.classList='col-12 d-none'
+    myRewardsContent.classList='col-12'
+    
+    myRewardsTab.classList.remove('bg-trans-white')
+    myRewardsTab.classList.add('bg-white','fw-bold')
+
+    rewardsTab.classList.remove('bg-white','fw-bold')
+    rewardsTab.classList.add('bg-trans-white')     
+}
+
+
+
 
 
