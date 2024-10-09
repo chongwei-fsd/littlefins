2d0dc3a239f4f4521f462c1cac9fa180dc50b1ca
chongwei-fsd <ChongWei@genstudents.org>
2024-10-09 10:41:30 +0800

    Add files via upload

---
+++ backend/littlefins.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE  IF NOT EXISTS `littlefins` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `littlefins`;
+-- MySQL dump 10.13  Distrib 8.0.33, for macos13 (arm64)
+--
+-- Host: localhost    Database: littlefins
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `redeemed`
+--
+
+DROP TABLE IF EXISTS `redeemed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `redeemed` (
+  `date_used` date DEFAULT NULL,
+  `is_redeemed` bit(1) DEFAULT NULL,
+  `is_used` bit(1) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `voucher_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK2fpx554407iv3orxrf7o7d63i` (`voucher_id`),
+  KEY `FKpmuu94ah7adbv7u3dwcvydhc4` (`user_id`),
+  CONSTRAINT `FK7do0akh78ejfbkn5rmhr7tebc` FOREIGN KEY (`voucher_id`) REFERENCES `voucher` (`id`),
+  CONSTRAINT `FKpmuu94ah7adbv7u3dwcvydhc4` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `redeemed`
+--
+
+LOCK TABLES `redeemed` WRITE;
+/*!40000 ALTER TABLE `redeemed` DISABLE KEYS */;
+INSERT INTO `redeemed` VALUES (NULL,_binary '\0',_binary '\0',1,3,2),(NULL,_binary '\0',_binary '\0',2,2,1),(NULL,_binary '\0',_binary '\0',3,2,4);
+/*!40000 ALTER TABLE `redeemed` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `dob` date DEFAULT NULL,
+  `coin` bigint DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('1990-02-02',5,1,'nancy@gmail.com','nancy123','nancy'),('1990-03-03',5,2,'bong@gmail.com','bong123','bong'),('1990-04-04',5,3,'josh@gmail.com','josh123','josh'),('1990-05-05',5,4,'michelle@gmail.com','michelle123','michelle');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voucher`
+--
+
+DROP TABLE IF EXISTS `voucher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `voucher` (
+  `exp_date` date DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` bigint DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voucher`
+--
+
+LOCK TABLES `voucher` WRITE;
+/*!40000 ALTER TABLE `voucher` DISABLE KEYS */;
+INSERT INTO `voucher` VALUES ('2024-12-31',1,50,'$5 Amazon eVoucher','amazon.jpg'),('2024-12-31',2,50,'$5 Popular eVoucher','popular.jpg'),('2024-12-31',3,50,'$5 Liho eVoucher','liho.jpg'),('2024-12-31',4,50,'$5 Subway eVoucher','subway.png'),('2024-12-31',5,50,'$5 CottonOn eVoucher','cottonon.png'),('2024-12-31',6,50,'$5 Finest eVoucher','finest.jpg'),('2024-12-31',7,50,'$5 Kidsamaze eVoucher','kidsamaze.png'),('2024-12-31',8,50,'$5 Lego eVoucher','lego.png'),('2024-12-31',9,50,'$5 Macdonald eVoucher','mcd.jpeg'),('2024-12-31',10,50,'$5 Smiggle eVoucher','smiggle.jpg');
+/*!40000 ALTER TABLE `voucher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-09 10:40:08
