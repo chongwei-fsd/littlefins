34769b5ab5edc08fa17b3c14919474d05bd50027
chongwei-fsd <ChongWei@genstudents.org>
2024-10-11 18:37:41 +0800

    remove backend

--- backend/src/main/java/com/cw/littlefins_proj/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.cw.littlefins_proj.repo;
-
-import com.cw.littlefins_proj.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-}
