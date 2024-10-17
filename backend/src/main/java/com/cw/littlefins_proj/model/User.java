42c9789ce64b50ad0d37a8fae081cb3c255bc815
chongwei-fsd <ChongWei@genstudents.org>
2024-10-18 06:59:51 +0800

    Update User.java

--- backend/src/main/java/com/cw/littlefins_proj/model/User.java
+++ backend/src/main/java/com/cw/littlefins_proj/model/User.java
@@ -43,7 +43,7 @@ public class User implements UserDetails {
     private Long coin=0L;
 
     @Column
-    private LocalDateTime dob;
+    private LocalDate dob;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
