e7428fe9f980a8c4118d1a531103be84481e8107
chongwei-fsd <ChongWei@genstudents.org>
2024-10-21 12:51:43 +0800

    remove backend

--- backend/src/main/java/com/cw/littlefins_proj/exception/PasswordBlankException.java
+++
@@ -1,7 +0,0 @@
-package com.cw.littlefins_proj.exception;
-
-public class PasswordBlankException extends RuntimeException {
-    public PasswordBlankException() {
-        super("Password cannot be blank.");
-    }
-}
--- backend/src/main/java/com/cw/littlefins_proj/exception/UnspecifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.cw.littlefins_proj.exception;
-
-public class UnspecifiedException extends RuntimeException {
-    public UnspecifiedException() {
-        super("An unspecified error occurred. Please try again later.");
-    }
-}
